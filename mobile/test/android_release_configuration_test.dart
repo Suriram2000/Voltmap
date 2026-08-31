91d51a92c3b2e562afb42062483c9f2a326d4d5f
naveenadyasani <103554572+naveenadyasani@users.noreply.github.com>
2026-08-30 19:25:05 -0700

    Use supplied charger visual across release build 38

--- mobile/test/android_release_configuration_test.dart
+++ mobile/test/android_release_configuration_test.dart
@@ -93,7 +93,7 @@ void main() {
       expect(gradle, isNot(contains('signingConfigs.getByName("debug")')));
       expect(
         File('pubspec.yaml').readAsStringSync(),
-        contains('version: 1.15.7+37'),
+        contains('version: 1.15.7+38'),
       );
     });
   });
